dcd4fec2e93a37b50cf2d027cd7ae1220a574dd5
francing-p <ngfranciscus@gmail.com>
2024-09-25 11:33:31 -0400

    Commit

--- src/theater.java
+++ src/theater.java
@@ -8,6 +8,7 @@ public class theater {
         System.out.print("What is your age? ");
         if (in.hasNextInt()){ // checks to make sure integer was input
             age=in.nextInt(); // grabs user input
+            in.nextLine(); // clear buffer
             if (age>=21){ // gives wristband if 21 or older
                 System.out.println("You get a wristband! ");
             }
